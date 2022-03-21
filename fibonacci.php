c1ff768460c0a94404a2b4bdb71aa41986aa4aa1
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-03-21 10:39:27 +0900

    submit_no1

--- fibonacci.php
+++ fibonacci.php
@@ -1,2 +1,13 @@
 <?php
-// TODO
+$arr = [0,1];
+
+for($i=2; $i<= 10000; $i++)
+{
+  $arr[$i] = $arr[$i-2] + $arr[$i-1];
+  if($arr[$i] > 10000){
+    $arr[$i+1] = "";
+    break;
+  }
+}
+
+echo implode("\n", $arr);
